50d2790d0e3a8763668f25585f23aa0816b56fcc
kananmhn <kannanfrlearn@gmail.com>
2024-09-09 16:29:17 +0530

    lazy load with suspense

---
+++ app/components/LazyComponent.tsx
@@ -0,0 +1,12 @@
+// components/LazyComponent.tsx
+import React from 'react';
+
+const LazyComponent: React.FC = () => {
+  return (
+    <div>
+      <h1>This is a lazy-loaded component!</h1>
+    </div>
+  );
+}
+
+export default LazyComponent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,38 @@
-"use client"; // This directive marks the file as a client component
+// "use client"; // This directive marks the file as a client component
+// import React, { Suspense } from 'react';
+// import MyComponentWithSuspense from './components/MyCompo';
+
+// const Home: React.FC = () => {
+//   return (
+//     <div>
+//       <h1>Welcome to Next.js with React Suspense</h1>
+//         <MyComponentWithSuspense />
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+// pages/index.tsx
 import React, { Suspense } from 'react';
-import MyComponentWithSuspense from './components/MyCompo';
+import Loading from './Loading';
+
+// Function to simulate a delay
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+// Lazy load the component with a delay
+const LazyComponent = React.lazy(() =>
+  delay(2000).then(() => import('./components/LazyComponent'))
+);
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to Next.js with React Suspense</h1>
-        <MyComponentWithSuspense />
+    <div className="Home">
+        <Suspense fallback={<Loading />}>
+          <LazyComponent />
+        </Suspense>     
     </div>
   );
-};
+}
 
 export default Home;
